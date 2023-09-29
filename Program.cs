618e9235018925fce5c93141e01fc87b3a00b5db
JPreault <99177462+JPreault@users.noreply.github.com>
2023-09-29 10:14:31 +0200

    project

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        string? sentence = Console.ReadLine();
+        if(sentence is not null && sentence.Trim() != "") {
+            Console.WriteLine(ConvertToUpper(sentence));
+        } else {
+            Console.WriteLine("T'as pas rentré de chaîne de caractère, t'es neuneu ou quoi ?");
+        }
+    }
+
+    public static string ConvertToUpper(string text)
+    {
+       return text.ToUpper();
+    }
+}
